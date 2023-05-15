26cc26bab898d7027d07f73c6ad0b86034ce9cd7
Mrowr Purr <mrowr.purr@gmail.com>
2023-05-14 21:07:37 -0700

    <>

--- Editor/Editor.cpp
+++ Editor/Editor.cpp
@@ -2,6 +2,6 @@
 _LogToFile_("Editor.log");
 //
 
-#include "Camps1te\Editor\Main.h"
+#include <Camps1te/Editor/Main.h>
 
 int main(int argc, char* argv[]) { return Camps1te::Editor::Main(argc, argv); }
