37464d96a769c1daf6c81dafaf47df7f4645d97a
Mrowr Purr <mrowr.purr@gmail.com>
2023-05-14 17:58:15 -0700

    Add dependencies (json, string format, log)

--- Editor/Editor.cpp
+++ Editor/Editor.cpp
@@ -1,3 +1,7 @@
+#include <_Log_.h>
+_LogToFile_("Editor.log");
+//
+
 #include "Camps1te\Editor\Main.h"
 
 int main(int argc, char* argv[]) { return Camps1te::Editor::Main(argc, argv); }
--- Editor/xmake.lua
+++ Editor/xmake.lua
@@ -1,5 +1,8 @@
+add_requires("nlohmann_json", "string_format", "_Log_", "spdlog")
+
 target("Camps1te Editor")
     set_kind("binary")
     add_files("*.cpp", "../Resources/Editor.qrc")
     add_includedirs("include")
     add_qt()
+    add_packages("nlohmann_json", "string_format", "_Log_", "spdlog")
